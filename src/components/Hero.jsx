123f3766b2d6ab96b005e012d5939e155484d3ca
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-10-02 19:32:22 +0600

    modified files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='w-full h-dvh bg-gradient-to-r from-[#035762] to-[#3fd0c9]'>
+
+    </div>
+  )
+}
+
+export default Hero
